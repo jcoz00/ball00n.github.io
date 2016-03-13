29fe1876984b84e3ec4974d48d77f88b25dcccfe
BuildTools <jamescozzi00@gmail.com>
2016-03-12 21:42:25 -0600

    changes

--- addtohomescreen.js
+++ addtohomescreen.js
@@ -473,7 +473,7 @@ ath.Class.prototype = {
 		// try to get the highest resolution application icon
 		if ( !this.applicationIcon ) {
 			if ( ath.OS == 'ios' ) {
-				this.applicationIcon = document.querySelector('head link[rel^=apple-touch-icon][sizes="152x152"],head link[rel^=apple-touch-icon][sizes="144x144"],head link[rel^=apple-touch-icon][sizes="120x120"],head link[rel^=apple-touch-icon][sizes="114x114"],head link[rel^=apple-touch-icon]');
+				this.applicationIcon = document.querySelector('head link[rel^=apple-touch-icon][sizes="152x152"],head link[rel^=apple-touch-icon][sizes="120x120"],head link[rel^=apple-touch-icon][sizes="76x76"],head link[rel^=apple-touch-icon][sizes="60x60"],head link[rel^=apple-touch-icon]');
 			} else {
 				this.applicationIcon = document.querySelector('head link[rel^="shortcut icon"][sizes="196x196"],head link[rel^=apple-touch-icon]');
 			}
